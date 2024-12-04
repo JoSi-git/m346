9f7f9814db5a2fd8881e10096b696228d0103f38
arkaizn <silas.gubler@hispeed.ch>
2024-12-04 11:27:07 +0100

    test

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,4 +8,9 @@ aws ec2 authorize-security-group-ingress --group-name djs-sec-group
 aws ec2 authorize-security-group-ingress --group-name djs-sec-group
 --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name djs-key --security-groups djs-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Webserver}]'
+(
+    cd ./ec2webserver
+    aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name djs-key --security-groups djs-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Webserver}]'
+
+)
+
