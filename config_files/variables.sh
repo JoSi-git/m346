eea39aee4288aeac14ca4c35c65883aa4682aea4
JoSi <jonas.sieber@edu.gbssg.ch>
2024-12-17 23:32:47 +0100

    Update variables.sh

--- config_files/variables.sh
+++ config_files/variables.sh
@@ -4,5 +4,4 @@
 SLEEP_DURATION="20"                 # Zeit in Sekunden
 KEY_NAME="djs-key"                  #SSH Schlüsselname
 SEC_GROUP_NAME="djs-sec-group"      #AWS Security Group Name
-
-CONFIG_STEP=1                       # Fortschritt der Konfiguration: 1 (Instanz 1), 2 (Instanz 2), done (fertig)
+CONFIG_STEP=1                       # Fortschritt der Konfiguration:
