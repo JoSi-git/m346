03a7d59480c4463e0592d82e6478a488d5abef4d
arkaizn <silas.gubler@hispeed.ch>
2024-12-17 23:47:59 +0100

    add deletion of variables.sh

--- config_files/variables.sh
+++ config_files/variables.sh
@@ -3,8 +3,4 @@
 SLEEP_DURATION="20"                 # Zeit in Sekunden
 KEY_NAME="djs-key"                  #SSH Schlüsselname
 SEC_GROUP_NAME="djs-sec-group"      #AWS Security Group Name
-<<<<<<< HEAD
 CONFIG_STEP=1                       # Fortschritt der Konfiguration: 1 (Instanz 1), 2 (Instanz 2), done (fertig)
-=======
-CONFIG_STEP=1                       # Fortschritt der Konfiguration:
->>>>>>> eea39aee4288aeac14ca4c35c65883aa4682aea4
