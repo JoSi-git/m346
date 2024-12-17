f76f3923db4bab6bd80cf276c6e0e572f396f8e7
arkaizn <silas.gubler@hispeed.ch>
2024-12-18 00:53:48 +0100

    add mysql isntall

--- config_files/mysqlinstall.sh
+++ config_files/mysqlinstall.sh
@@ -8,5 +8,21 @@ source /home/ubuntu/variables.sh
 echo "Updating package list..."
 sudo apt update -y
 
-sudo apt install mysql-server -y
-sudo systemctl start mysql.service
+# MySQL Root Passwort und wpadmin Passwort
+MYSQL_ROOT_PASSWORD="Riethuesli>12345"
+MYSQL_WP_ADMIN_USER_PASSWORD="Riethuesli>12345"
+
+# MySQL Sicherung und Konfiguration
+mysql_secure_installation -u root --password="${MYSQL_ROOT_PASSWORD}" --use-default
+
+# MySQL Benutzer und Datenbank erstellen
+mysql -u root --password="${MYSQL_ROOT_PASSWORD}" <<EOF
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER 'wpadmin'@'%' IDENTIFIED BY '${MYSQL_WP_ADMIN_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wpadmin'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+# Konfiguration für Remote-Verbindungen
+echo "bind-address = 0.0.0.0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql restart
