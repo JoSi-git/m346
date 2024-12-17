42577ed81b6ded18ec6c6eddc16d4fad2e6c2639
arkaizn <silas.gubler@hispeed.ch>
2024-12-17 23:21:30 +0100

    fix false variables

--- scripts/initialize-web-instance.sh
+++ scripts/initialize-web-instance.sh
@@ -21,11 +21,11 @@ fi
 echo "Starte Webserver EC2-Instanz..."
 export AWS_PAGER=""
 
-INSTANCE_ID1=$(aws ec2 run-instances \
+INSTANCE_ID2=$(aws ec2 run-instances \
 --image-id ami-08c40ec9ead489470 \
 --count 1 \
 --instance-type t2.micro \
---key-name djs-key \
+--key-name $KEY_NAME \
 --security-groups $SEC_GROUP_NAME \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Webserver}]' \
 --query 'Instances[0].InstanceId' \
@@ -59,8 +59,8 @@ echo "Gefundene Public IP: $PUBLIC_IP2"
 # SSH-Verbindung herstellen und wpinstall.sh ausführen
 echo "Kopiere das wpinstall.sh-Skript auf die Instanz..."
 # Kopiere das Skript auf die Instanz
-scp -i ~/.ssh/djs-key.pem -o StrictHostKeyChecking=accept-new ./config_files/wpinstall.sh ubuntu@"$PUBLIC_IP2":/home/ubuntu/wpinstall.sh
-scp -i ~/.ssh/djs-key.pem -o StrictHostKeyChecking=accept-new ./config_files/variables.sh ubuntu@"$PUBLIC_IP2":/home/ubuntu/variables.sh
+scp -i ~/.ssh/$KEY_NAME.pem -o StrictHostKeyChecking=accept-new ./config_files/wpinstall.sh ubuntu@"$PUBLIC_IP2":/home/ubuntu/wpinstall.sh
+scp -i ~/.ssh/$KEY_NAME.pem -o StrictHostKeyChecking=accept-new ./config_files/variables.sh ubuntu@"$PUBLIC_IP2":/home/ubuntu/variables.sh
 
 # Prüfe, ob der Upload erfolgreich war
 if [ $? -ne 0 ]; then
@@ -73,7 +73,7 @@ echo "--------------------------------------------------------------------------
 
 # Führe das WordPress-Installationsskript auf der Instanz aus
 echo "Führe das WordPress-Installationsskript auf der Instanz aus..."
-ssh -i ~/.ssh/djs-key.pem -o StrictHostKeyChecking=accept-new ubuntu@"$PUBLIC_IP2" << 'EOF'
+ssh -i ~/.ssh/$KEY_NAME.pem -o StrictHostKeyChecking=accept-new ubuntu@"$PUBLIC_IP2" << 'EOF'
     echo "Setze Berechtigungen für wpinstall.sh.."
     chmod +x /home/ubuntu/wpinstall.sh
     echo "Starte die Ausführung von wpinstall.sh..."
