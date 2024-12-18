67e9f9cd8dec10396f57d8f90f220c9d134dcd82
arkaizn <silas.gubler@hispeed.ch>
2024-12-18 20:07:04 +0100

    fix echo into file

--- config_files/variables.sh
+++ config_files/variables.sh
@@ -1,7 +1,7 @@
 SLEEP_DURATION="20"
 KEY_NAME="djs-key"
 SEC_GROUP_NAME="djs-sec-group"
-DB_NAME="wordpress"
-DB_USER="wp-user"
-DB_PASSWORD="Riethuesli>12345"
 CONFIG_STEP=1
+DB_NAME=wordpress
+DB_USER=wp-user
+DB_PASSWORD=Riethuesli
